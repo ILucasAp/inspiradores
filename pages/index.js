6aae0dc16e2e530c4f818ca7159c21a90bf4f944
Lucas Pontes <111016726+ILucasAp@users.noreply.github.com>
2025-06-04 23:46:41 +0000

    Modifica  a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Você surgiu para iluminar minha vida, obrigado por fazer parte da dessa nossa jornada!</h1>
+    return <h1>Você surgiu para iluminar minha vida, obrigado por fazer parte da dessa nossa jornada! 🥰</h1>
 }
 
 export default Home;
