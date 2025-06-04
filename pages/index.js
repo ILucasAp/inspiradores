a50bad230479471e3341e966e394f26f68015b07
Lucas Pontes <111016726+ILucasAp@users.noreply.github.com>
2025-06-04 23:54:59 +0000

    Mudando a franse novamente

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Você surgiu para iluminar minha vida, obrigado por fazer parte da dessa nossa jornada! 🥰</h1>
+    return <h1>Testando essa bagaça!!! 🥰</h1>
 }
 
 export default Home;
